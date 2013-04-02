d25ac86d80c994ec31951f5139ca9588885f11a4
maxanier <maxanier@gmx.de>
2013-04-02 22:48:09 +0200

    Versionskontrolle hinzugefuegt

--- src/com/example/vertretungsplan/MainActivity.java
+++ src/com/example/vertretungsplan/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.vertretungsplan;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -10,11 +14,19 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 	public static final String TAG = "Main_Acticity";
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		UpdateCheck check = new UpdateCheck(this);
+		try{PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+		Double version = Double.parseDouble(pInfo.versionName);
+		Log.i(TAG,"Version: "+version);
+		check.execute(version);}
+		catch(Exception e){}
 		
 		//final Button plan_button = (Button) findViewById(R.id.button1);
 	}
@@ -39,5 +51,6 @@ public class MainActivity extends Activity {
 		i.setClass(this, Options.class);
 		startActivity(i);
 	}
+
 
 }
